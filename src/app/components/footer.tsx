dc0c726747ce4abf1b726f075d71fdd99dc73371
seanaxn1135 <seanaxn1135@gmail.com>
2024-02-18 18:20:38 +0800

    Update social links and let link open in new tab

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,24 +1,25 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+import Link from 'next/link';
 
 export default function Footer() {
     const currentYear = new Date().getFullYear();
     const socialLinks = [
         {
-            href: "https://github.com/yourGithubProfile",
+            href: "https://github.com/seanaxn1135",
             icon: faGithub,
             label: "GitHub",
             color: "text-black dark:text-white hover:text-gray-500",
         },
         {
-            href: "https://www.linkedin.com/in/yourLinkedInProfile",
+            href: "https://www.linkedin.com/in/seanaxn",
             icon: faLinkedin,
             label: "LinkedIn",
             color: "text-blue-600 hover:text-blue-700",
         },
         {
-            href: "mailto:yourEmail@example.com",
+            href: "mailto:seanaxn1135@gmail.com",
             icon: faEnvelope,
             label: "Email",
             color: "text-red-600 hover:text-red-700",
@@ -31,9 +32,11 @@ export default function Footer() {
 
             <div className="flex justify-center space-x-6">
                 {socialLinks.map((link) => (
-                    <a key={link.label} href={link.href} className={link.color} aria-label={link.label}>
+                    <Link key={link.label} href={link.href} className={link.color} aria-label={link.label} target="_blank"
+                        rel="noopener noreferrer">
                         <FontAwesomeIcon icon={link.icon} />
-                    </a>
+
+                    </Link>
                 ))}
             </div>
 
