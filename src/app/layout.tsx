175b0689bca75c94ddabcc523f43b2fa03abdda7
seanaxn1135 <seanaxn1135@gmail.com>
2024-03-19 18:12:21 +0800

    add content to homepage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ const inter = Inter({
 
 export const metadata: Metadata = {
   title: "Sean Ang",
-  description: "Engineer, Gamer and aspiring Writer",
+  description: "Software Engineer, Gamer and aspiring Blogger",
 };
 const cx = (...classes: any[]) => classes.filter(Boolean).join(' ');
 
@@ -25,7 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={cx(inter.className, 'text-black bg-white dark:text-white dark:bg-[#111010]')}>
-      <body className="antialiased max-w-2xl mb-40 flex-col md:flex-row mx-4 mt-8 lg:mx-auto">
+      <body className="antialiased max-w-2xl mb-4 flex-col md:flex-row mx-4 mt-8 lg:mx-auto">
 
         <Navbar />
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,42 @@
 import Link from "next/link";
-import { JSX, ClassAttributes, AnchorHTMLAttributes } from "react";
-
-function Badge(props: JSX.IntrinsicAttributes & ClassAttributes<HTMLAnchorElement> & AnchorHTMLAttributes<HTMLAnchorElement>) {
-  return (
-    <a
-      {...props}
-      target="_blank"
-      className="border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 rounded p-1 text-sm inline-flex items-center leading-4 text-neutral-900 dark:text-neutral-100 no-underline"
-    />
-  );
-}
 
 export default function Page() {
   return (
-    <section>
-      <h1 className="font-medium text-2xl mb-8 tracking-tighter">hey, I'm Sean 👋 </h1>
-      <p className="prose dark:prose-invert">{`I'm a fullstack developer, avid `}
-        <Link target="_blank" href="https://steamcommunity.com/id/ribbitrabbid/" rel="noopener noreferrer">gamer</Link>
-        {` and an aspiring blog writer. I currently work as a Consultant Developer for `}
-        <Link target="_blank" href="https://www.thoughtworks.com/" rel="noopener noreferrer">ThoughtWorks</Link>{` , where I collaborate with brilliant minds to deliver impactful digital products.`}</p>
-    </section>
-  )
+    <>
+      <section>
+        <h1 className="font-medium text-2xl mb-6 tracking-tighter">hey, I'm Sean 👋</h1>
+        <p className="prose dark:prose-invert">
+          {`I'm a full-stack developer, avid `}
+          <Link target="_blank" href="https://steamcommunity.com/id/ribbitrabbid/" rel="noopener noreferrer">gamer</Link>
+          {` and an aspiring blog writer. I currently work as a Consultant Developer for `}
+          <Link target="_blank" href="https://www.thoughtworks.com/" rel="noopener noreferrer">ThoughtWorks</Link>
+          {`, where I collaborate with brilliant minds to deliver impactful digital products. When I'm not coding, you'll likely find me diving into the latest game release or lost in thought, piecing together my next blog post. Gaming is not just a hobby for me; it's a gateway to new worlds and stories, much like software engineering is a portal to endless possibilities and innovations.`}
+        </p>
+        <div className="prose prose-neutral dark:prose-invert mt-10">
+          <h2 className="font-small text-xl tracking-tighter">why I write ✍️</h2>
+          <p className="prose dark:prose-invert">
+            My blog is my battleground for demystifying software engineering—transforming complex concepts into digestible, simple-to-understand bites. And here's why I'm passionate about it:
+          </p>
+          <ul className="list-disc ml-5 mb-4">
+            <li>
+              <strong>To empower new engineers 💪</strong> - The tech world is vast and sometimes intimidating. I believe in nurturing a new generation of engineers who are equipped to tackle tomorrow's challenges.
+            </li>
+            <li>
+              <strong>To test my understanding 💭</strong> - By breaking down complex topics, I not only share knowledge but also solidify my own understanding.
+            </li>
+            <li>
+              <strong>Because I've been there 🧑‍🏫</strong> - The best teachers often aren't the distant experts but those who've recently walked the path themselves. Last year's learner is this year's guide, and I aim to be that bridge for others.
+            </li>
+          </ul>
+        </div>
+
+        <div className="prose prose-neutral dark:prose-invert mt-12">
+          <h2 className="font-small text-xl tracking-tighter">let's connect 🌐</h2>
+          <p className="prose dark:prose-invert">
+            Whether you're here to share insights, seek guidance, or simply say hi, I'm all ears. My journey is about more than just technology; it's about connections, learning, and growing together. So, if you're curious about software engineering, looking for tips, or just want to talk about the latest game that's captured my heart, you're in the right place.
+          </p>
+        </div>
+      </section>
+    </>
+  );
 }
